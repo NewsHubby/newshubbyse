6745f274cc2a5b47651e7332f86d5bfd3f734597
Roland Johansson <rolandschelling@gmail.com>
2015-10-26 15:18:47 +0100

    Buggfixar, copyändringar

--- app/assets/javascripts/presto.js
+++ app/assets/javascripts/presto.js
@@ -3,6 +3,14 @@
 
 $(document).ready(function() {
 
+    //denna funktion ligger här bara tillfälligt. Ska egentligen ligga in global.js men jag lyckas inte göra så att global.js laddas in. /Roland
+    // Display uploaded file filename 
+    if (document.getElementById("uploadBtn")) {
+        document.getElementById("uploadBtn").onchange = function () {
+            document.getElementById("uploadFile").value = this.value.replace(/^.*\\/, "");
+        };
+    }
+
     var i = 1; // counter starting at 1
     var qCount = $('.question').length; // count the number of .question divs
     //console.log('There are ' + qCount + ' question divs present'); // log number of .question in console. Sanity check.
